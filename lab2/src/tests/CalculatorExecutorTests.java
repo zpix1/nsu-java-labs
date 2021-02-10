cb8d26a7dcb423a9b6185d979931683c0ded1fea
Ivan Baksheev <zpix-dev@list.ru>
2021-02-10 16:04:32 +0700

    lab2: comment fix

--- lab2/src/tests/CalculatorExecutorTests.java
+++ lab2/src/tests/CalculatorExecutorTests.java
@@ -11,7 +11,7 @@ import java.io.*;
 public class CalculatorExecutorTests {
     @Test
     public void validProgramWorks() throws IOException {
-        var instructions = "DEFINE A 5\nDEFINE B 6\nPUSH A\nPUSH B\nPUSH 10\n+\nSQRT\n*\nPRINT";
+        var instructions = "#### Strange comment\nDEFINE A 5\nDEFINE B 6\nPUSH A\nPUSH B\nPUSH 10\n+\nSQRT\n*\nPRINT";
         var out = new ByteArrayOutputStream();
         var outWriter = new OutputStreamWriter(out);
         var executor = new Executor(new InputStreamReader(new ByteArrayInputStream(instructions.getBytes())), outWriter);
