4f379f6d498a9c8707546b33a0d11e09929a925d
Ivan Baksheev <zpix-dev@list.ru>
2021-02-27 21:01:35 +0700

    lab3: settings.properties sorted

--- lab3/src/main/java/ru/nsu/fit/ibaksheev/minesweeper/model/SettingsManager.java
+++ lab3/src/main/java/ru/nsu/fit/ibaksheev/minesweeper/model/SettingsManager.java
@@ -1,5 +1,6 @@
 package ru.nsu.fit.ibaksheev.minesweeper.model;
 
+import com.sun.source.tree.Tree;
 import ru.nsu.fit.ibaksheev.minesweeper.model.exceptions.InvalidArgumentException;
 
 import java.io.IOException;
@@ -7,25 +8,26 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 public class SettingsManager {
-    private final Map<String, GameData.Settings> settingsTypes;
+    private final SortedMap<String, GameData.Settings> settingsTypes;
 
     // TODO: Tried to use reflexion api here, but it's just evil
 
     public SettingsManager(String filename) {
         Properties props = new Properties();
-        settingsTypes = new HashMap<>();
+        settingsTypes = new TreeMap<>();
         try {
             props.load(this.getClass().getResourceAsStream(filename));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (var propName: Collections.list(props.keys())) {
-            var splitted = propName.toString().split("\\.");
-            var type = splitted[0];
-            var fieldName = splitted[1];
+        for (var propName: Collections.list(props.keys()).stream().sorted(Comparator.comparing(Object::toString)).toArray()) {
+            var splitProp = propName.toString().split("\\.");
+            var type = splitProp[0];
+            var fieldName = splitProp[1];
             if (settingsTypes.get(type) == null) {
                 settingsTypes.put(type, new GameData.Settings(0, 0, 0));
             }
+            System.out.println(propName);
             switch (fieldName) {
                 case "name":
                     settingsTypes.get(type).setName((String) props.get(propName));
